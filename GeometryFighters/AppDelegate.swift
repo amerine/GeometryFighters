94801fe768ce0d98e934dba2f26cfce55de07935
Mark Turner <mark@amerine.net>
2017-12-22 20:10:12 -0800

    Initial Commit

---
+++ GeometryFighters/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GeometryFighters
+//
+//  Created by Mark Turner on 12/22/17.
+//  Copyright © 2017 Mark Turner. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
